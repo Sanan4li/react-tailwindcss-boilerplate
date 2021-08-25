dd7e34a49d5f0b6635bb441088955b03b7ac0992
SananAli <sanan.ali@bytemage.io>
2021-08-25 16:24:45 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div>
+      <h1 className="text-center text-4xl p-2 m-2 font-semibold">
+        React App with Tailwind CSS
+      </h1>
+      <p className="p-2 text-center ">
+        Welcome to tailwind css with react. This is a boilerplate created with
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="text-blue-400 p-1"
+          href="https://reactjs.org/docs/create-a-new-react-app.html"
         >
-          Learn React
+          Create React App
         </a>
-      </header>
+        and
+        <a className="text-blue-400 p-1" href="https://tailwindcss.com/">
+          Tailwind CSS
+        </a>
+        .
+      </p>
+      <h2 className="text-3xl p-1 mt-3 text-center font-semibold">
+        {" "}
+        How to use ?
+      </h2>
+      <p className="text-center">
+        After cloning the repository. You can use the following commands to use
+        this boilerplate.
+      </p>
+      <ul className="text-center mt-3">
+        <li>
+          <span className="font-semibold text-xl">npm start </span> : start the
+          project
+        </li>
+        <li>
+          <span className="font-semibold text-xl">npm build </span> :build the
+          project
+        </li>
+        <li>
+          <span className="font-semibold text-xl">npm run build-css </span>:
+          build the output css file '<i>src/assets/css/index.css</i>'
+        </li>
+        <li>
+          <span className="font-semibold text-xl">npm run watch-css </span>: JIT
+          mode watch for changes and build the output css file '
+          <i>src/assets/css/index.css</i>'
+        </li>
+      </ul>
     </div>
   );
 }
